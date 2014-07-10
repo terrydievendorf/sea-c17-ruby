a78b6be3563999bdd8023f9fadc18e811be58416
Ryan Sobol <contact@ryansobol.com>
2014-07-10 14:15:49 -0700

    Add questions from chapter 6

---
+++ class2/7_angry_boss.rb
@@ -0,0 +1,7 @@
+# Section 6.2 on page 32
+# 4 points
+# Write an angry boss program that rudely asks what you want. Whatever you
+# answer, the angry boss should yell it back to you and then fire you. For
+# example, if you type in I want a raise, it should yell back like this:
+#
+# WHADDAYA MEAN "I WANT A RAISE"?!? YOU'RE FIRED!!
---
+++ class2/8_table_of_contents.rb
@@ -0,0 +1,11 @@
+# Section 6.2 on page 32
+# 4 points
+# Here’s something for you to do in order to play around more with center,
+# ljust, and rjust: write a program that will display a table of contents so
+# that it looks like this:
+#
+#                 Table of Contents
+#
+# Chapter 1:  Getting Started                page  1
+# Chapter 2:  Numbers                        page  9
+# Chapter 3:  Letters                        page 13
