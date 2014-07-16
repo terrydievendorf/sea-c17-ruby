318c9e936c001c2fea32b2e75f7ebbee5086e2f3
Ryan Sobol <contact@ryansobol.com>
2014-07-16 02:48:09 -0700

    Add sample output to class 3 exercise 1

--- class3/1_99_bottles_of_beer_on_the_wall.rb
+++ class3/1_99_bottles_of_beer_on_the_wall.rb
@@ -2,4 +2,14 @@
 # 4 points
 #
 # Write a program that prints out the lyrics to that beloved classic,
-# "99 Bottles of Beer on the Wall."
+# "99 Bottles of Beer on the Wall." Here's what the output should look like:
+#
+# 99 bottles of beer on the wall, 99 bottles of beer!
+# Take one down, pass it around, 98 bottles of beer on the wall!
+# 98 bottles of beer on the wall, 98 bottles of beer!
+# Take one down, pass it around, 97 bottles of beer on the wall!
+# ...
+# 2 bottles of beer on the wall, 2 bottles of beer!
+# Take one down, pass it around, 1 bottle of beer on the wall!
+# 1 bottle of beer on the wall, 1 bottle of beer!
+# Take one down, pass it around, no more bottles of beer on the wall!
