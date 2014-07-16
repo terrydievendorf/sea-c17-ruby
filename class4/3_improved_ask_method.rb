4a4135875e803ada62e1cdf8b05ed83dc6499b45
Ryan Sobol <contact@ryansobol.com>
2014-07-16 16:25:32 -0700

    Add class 4 assignment stubs

---
+++ class4/3_improved_ask_method.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+# Section 9.5 on page 68
+# 4 points
+#
+# That ask method I showed you on page 66 was OK, but I bet you could do better.
+# Try to clean it up by removing the answer variable. You’ll have to use return
+# to exit from the loop. (Well, it will get you out of the whole method, but it
+# will get you out of the loop in the process.)
+#
+# Hint: I've already structured your program. Aren't you lucky? :)
+
+def ask(question)
+  # your solution here
+end
+
+puts ask('Do you like eating tacos?')
