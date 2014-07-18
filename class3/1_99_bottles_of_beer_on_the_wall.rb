3ba6dfc969e83ebff6e60ad07aaffd65dfa1f989
Terry Dievendorf <terrydievendorf@yahoo.com>
2014-07-17 18:49:33 -0700

    Changed some text

--- class3/1_99_bottles_of_beer_on_the_wall.rb
+++ class3/1_99_bottles_of_beer_on_the_wall.rb
@@ -24,6 +24,6 @@ while current_bottle_count > 1
   current_bottle_count -= 1
 end
 puts "1 bottle of beer on the wall, 1 bottle of beer!"
-puts "Take one down and pass it around, no more bottles of beer on the wall!"
+puts "Take one down, pass it around, no more bottles of beer on the wall!"
 puts "No more bottles of beer on the wall, no more bottles of beer!"
 puts "Go to the store and buy some more, 99 bottles of beer on the wall!"
